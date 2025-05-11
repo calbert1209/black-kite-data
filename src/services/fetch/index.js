7446641c3ae67554167823ca46d5b0df65b79e78
Christian Albert <calbert1209@gmail.com>
2025-05-11 16:56:32 +0900

    add fetch and parse fns for tidal data

--- src/services/fetch/index.js
+++ src/services/fetch/index.js
@@ -11,3 +11,15 @@ export async function fetchTideStationData() {
   const response = await axios.get(STATION_PAGE_URL);
   return response.data;
 }
+
+/**
+ *
+ * @param {string} stationCode
+ * @param {number | string} year
+ * @returns {Promise<string>} The JMA tide table data for the specified station and year.
+ */
+export async function fetchTideTableData(stationCode, year) {
+  const url = `https://www.data.jma.go.jp/kaiyou/data/db/tide/suisan/txt/${year}/${stationCode}.txt`;
+  const response = await axios.get(url);
+  return response.data;
+}
---
+++ src/services/parse/tidal_events.js
@@ -0,0 +1,177 @@
+/**
+ * Parse hourly level measurements from row of table data.
+ *
+ * @param { string } row line of data from table to parse.
+ *
+ * @return { number[] } Array of 24 hourly level measurements.
+ */
+function parseHourlyData(row) {
+  const rawData = row.substring(0, 72);
+  const output = [];
+  for (let i = 0; i < 24; i++) {
+    const levelText = rawData.substring(i * 3, i * 3 + 3);
+    const level = parseInt(levelText, 10);
+    output.push(level);
+  }
+
+  return output;
+}
+
+/**
+ * @typedef { Object } RowDate
+ * @property { number } year
+ * @property { number } month (1-12)
+ * @property { number } day
+ *
+ * @typedef { RowDate } RowDateTime
+ * @property { number } year
+ * @property { number } month (1-12)
+ * @property { number } day
+ * @property { number } hour (0-23)
+ * @property { number } minutes (0-59)
+ */
+
+/**
+ * Parse date from row of table data.
+ *
+ * @param { string } row line of data from table to parse.
+ *
+ * @return { RowDate } Object with year, month, and day properties.
+ */
+function parseDate(row) {
+  const rawData = row.substring(72, 78);
+  const [year, month, day] = [0, 2, 4].map((cursor) => {
+    const digits = rawData.substring(cursor, cursor + 2);
+    return parseInt(digits, 10);
+  });
+  return {
+    year,
+    month,
+    day,
+  };
+}
+
+/**
+ * Parse station code from row of table data.
+ *
+ * @param { string } row line of data from table to parse.
+ *
+ * @returns { string } 2 character station code.
+ *
+ * @see JMA: {@link https://www.data.jma.go.jp/kaiyou/db/tide/suisan/station.php | 潮位表掲載地点一覧表（2025年）}
+ */
+function parseStationCode(row) {
+  return row.substring(78, 80);
+}
+
+/**
+ * A bare extremum object without any station code or type information.
+ * @typedef { Object } BareExtremum
+ * @property { number } hour hour measurement
+ * @property { number } minutes minutes measurement
+ * @property { number } level in cm
+ *
+ * A full extremum object with station code and type information.
+ * @typedef { Object } Extremum
+ * @property { RowDateTime } local_time
+ * @property { string } station_code
+ * @property { number } level in cm
+ * @property { string } type "high" or "low"
+ *
+ * An hourly level object with station code and time information.
+ * @typedef { Object } HourlyLevel
+ * @property { RowDateTime } local_time
+ * @property { string } station_code
+ * @property { number } level in cm
+ */
+
+/**
+ * Parse extrema (high- and low-tide events) from row of table data.
+ *
+ * @param {string} row line of data from table to parse.
+ *
+ * @param {number} start starting index of extrema data in row.
+ *
+ * @returns { BareExtremum[] } Array of bare extremum objects.
+ */
+function parseExtrema(row, start) {
+  const rawData = row.substring(start, start + 28);
+  return (
+    [0, 7, 14, 21]
+      .map((c) => {
+        const hText = rawData.substring(c, c + 2);
+        const mText = rawData.substring(c + 2, c + 4);
+        const level = rawData.substring(c + 4, c + 7);
+        return {
+          hour: parseInt(hText, 10),
+          minutes: parseInt(mText, 10),
+          level: parseInt(level, 10),
+        };
+      })
+      // filter out filler entries with all 9s
+      .filter((event) => event.hour !== 99)
+  );
+}
+
+/**
+ * Results of parsing a JMA tide table.
+ * @typedef {Object} ParsedTableData
+ * @property { HourlyLevel[] } hourlyLevels
+ * @property { Extremum[] } extrema
+ */
+
+/**
+ * Parse JMA tide table data for a specific station and year.
+ * @param {string} table JMA tide table data for a specific station and year.
+ * @returns {ParsedTableData} Parsed data containing hourly levels and extrema.
+ */
+export function parseTidalEvents(table) {
+  /** @type {HourlyLevel[]} */
+  const hourlyLevels = [];
+  /** @type {Extremum[]} */
+  const extrema = [];
+
+  const rows = table.split("\n");
+  for (let row of rows) {
+    const { year, month, day } = parseDate(row);
+    const station_code = parseStationCode(row);
+    const hourlyData = parseHourlyData(row);
+
+    hourlyData.forEach((level, index) => {
+      const local_time = { year, month, day, hour: index, minutes: 0 };
+      const levelData = { local_time, station_code, level };
+      hourlyLevels.push(levelData);
+    });
+
+    // Parse high tide events
+    parseExtrema(row, 80).forEach(({ hour, minutes, level }) => {
+      const local_time = { year, month, day, hour, minutes };
+      const extremum = {
+        local_time,
+        station_code,
+        level,
+        type: "high",
+      };
+
+      extrema.push(extremum);
+    });
+
+    // Parse low tide events
+    parseExtrema(row, 108).forEach(({ hour, minutes, level }) => {
+      const local_time = { year, month, day, hour, minutes };
+      const extremum = {
+        local_time,
+        station_code,
+        level,
+        type: "low",
+      };
+
+      extrema.push(extremum);
+    });
+  }
+
+  return {
+    hourlyLevels,
+    extrema,
+  };
+}
